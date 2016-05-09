3bce07bbed62f3b90a65440be577cd490f007e67
David Suarez <dsuarez4@terpmail.umd.edu>
2016-05-08 23:49:48 -0400

    WOW much better

--- scrapeAndDiff.py
+++ scrapeAndDiff.py
@@ -1,61 +1,46 @@
 #! /usr/bin/python
 
-#import requests.urlopen as urlopen
-#from requests import urlopen
-#from urllib.requests import urlopen
-#import urllib3
-import requests #pip install request==
-from BeautifulSoup import BeautifulSoup #fixed error of 'obj module is not callable
-from urlparse import urljoin 
+from base64 import b64encode, b64decode
+from bs4 import BeautifulSoup
+from email.mime.text import MIMEText
 from hashlib import sha256
+from smtplib import SMTP
+from urllib.parse import urljoin
+from urllib.request import urlopen
 
-import urllib
 '''
-try:
+    try:
     # For Python 3.0 and later
     from urllib.request import urlopen
-except ImportError:
+    except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
-
-html = urlopen("http://www.google.com/")
-print(html.read())
+    
+    html = urlopen("http://www.google.com/")
+    print(html.read())
 '''
 
-def grab_site_content(url):
-    print "Yo Fetching website"
-    
-    r = requests.get(url)
-    print r
-    soup = BeautifulSoup(r.content)
-    #links = [ a['href'] for a in soup.findAll('a',href=True)] 
-    links = [ urljoin(url, a['href']) for a in soup.findAll('a',href=True) ]
-    print links
-    #^^NEEDS WORKS
-    #Error when using find_all of 'Object not callable'
-    for page in links:
-        #Ignore the sitemap page
-        if page == '/site/csc110winter2015/system/app/pages/sitemap/hierarchy':
-            continue
-        #page_req_test = urlopen(page)
-        #print page_req_test
-        
-        with urllib.urlopen(page) as page_req:
-            fingerprint = sha256()
-            print fingerprint
-        
-    #for page in links:
-        
-    
-        
-    #Page encoding
-    
-    
-    
-        
-    print links
-    
-    #print soup 
+def summarize_site(index_url):
+    '''
+    Return a dict that maps the URL to the SHA-256 sum of its page contents
+    for each link in the index_url.
+    '''
+    summary = {}
+    with urlopen(index_url) as index_req:
+        soup = BeautifulSoup(index_req.read())
+        links = [urljoin(index_url, a.attrs.get('href'))
+                 for a in soup.select('li.topLevel a[href^=/site/csc110winter2015/]')]
+        for page in links:
+            # Ignore the sitemap page
+            if page == '/site/csc110winter2015/system/app/pages/sitemap/hierarchy':
+                continue    
+            with urlopen(page) as page_req:
+                fingerprint = sha256()
+                soup = BeautifulSoup(page_req.read())
+                for div in soup.find_all('div', class_='sites-attachments-row'):
+                    fingerprint.update(div.encode())
+                summary[page] = fingerprint.digest()
+    return summary
 
 def save_site_summary(filename, summary):
     with open(filename, 'wt', encoding='utf-8') as f:
@@ -124,7 +109,6 @@ def main(index_url, filename):
 
 
 
-#main(url='http://github.com/dsuarez4')
-main(url='https://sites.google.com/site/csc110winter2015/home')
+main(url='http://github.com/dsuarez4')
 
 #EOF
